b4634e99d89a49687096676a4ec3743f66162455
jonathangrant <jonathanallengrant@gmail.com>
2022-12-03 11:54:53 -0500

    ran nb

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +13,12 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 1.0
+# Average CV score on the training set was: 0.0
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    RobustScaler(),
+    FastICA(tol=0.45),
+    MaxAbsScaler(),
+    MLPClassifier(alpha=0.0001, learning_rate_init=0.01)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
